40f402f9f93c3afab1386abb10839ce7a98fbf2c
eliecer <eliecerdjmontoya@gmail.com>
2026-05-20 12:26:04 -0500

    segunda parte 1:04 minutos de clase

---
+++ tipos/02-strings .py
@@ -0,0 +1,10 @@
+nombre_curso = "ultimate python"
+descripcion_curso = """ultimate curso
+ este curso es pra aprender a prpgramar en python y adquirir un
+   buen trabajo"""
+print(nombre_curso, descripcion_curso)
+print(len(nombre_curso))
+print (nombre_curso[0])
+print (nombre_curso[:8])
+print (nombre_curso[9:])
+print (nombre_curso[:])
---
+++ tipos/03- formar -string .py
@@ -0,0 +1,4 @@
+nombre = "eliecer"
+apellido = "montoya"
+nombre_completo = f" {nombre} {apellido}"
+print(nombre_completo)
