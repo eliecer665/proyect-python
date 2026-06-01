8e3a1d45a0ab075bee1ad2b3348b2fa017b34958
eliecer <eliecerdjmontoya@gmail.com>
2026-06-01 07:54:57 -0500

    con app de calculadora

---
+++ tipos/07-numeros-funciones.py
@@ -0,0 +1,14 @@
+import math
+
+
+print(round(1.3)) # redondear un numero o aproximación
+print(round(1.7))
+print(abs(-1.3)) # vaor absoluto 
+print(abs(55))
+
+
+print(math.floor(1.9999))
+print(math.ceil(1.1))
+print(math.isnan(23)) # numero rela no puede estar en comillas
+print(math.pow(10, 2))  # se eleva apotencia
+print(math.sqrt(81)) # raiz cuadrada    se puede buscar en google las funciones de math
---
+++ tipos/calculadora.py
@@ -0,0 +1,21 @@
+n1 = input("ingresa tu primer numero") 
+n2 = input("ingresa el segundo numero")
+
+n1 = int(n1)
+n2 = int(n2)
+
+
+suma = n1 + n2
+resta = n1 - n2
+multi = n1 * n2
+div = n1 / n2
+
+mensaje = f"""
+para los numero {n1} y {n2},
+el resultado de la suma es {suma}.
+el resultado de la resta es {resta}.
+el resultado de la  multi es {multi}.
+el resultado de la  div es {div}.
+"""
+
+print(mensaje)
